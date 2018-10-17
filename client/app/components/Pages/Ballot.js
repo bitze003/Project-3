2ba9dc0f3bdb2c94382aa98bd4ad88fef8244eb3
Kenneth Bitzer <bitze003@d.umn.edu>
2018-10-17 00:17:00 -0500

    fixing con

--- client/app/components/Pages/Ballot.js
+++ client/app/components/Pages/Ballot.js
@@ -18,6 +18,9 @@ class Ballot extends Component {
 
   render() {
     return (
+
+      <div>
+      <Header />
         {/* <div style={{ backgroundColor: "#5781F6", paddingTop: 30, paddingBottom: 30 }}>
           <div style={{backgroundColor: "white", width:700, marginLeft: "auto", marginRight: "auto", paddingBottom: 20, borderRadius: 10}}> */}
             <Api />
