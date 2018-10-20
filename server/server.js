1193a4e4f3d922ead2b77c383f539ce9a5b784fc
wood0761 <wood0761@gmail.com>
2018-10-20 14:40:04 -0500

    serve static files from correct place

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ if (isDev) {
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.resolve(__dirname, '../dist')));
 } else {
-  app.use(express.static(path.resolve(__dirname, '../dist')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
   app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
     res.end();
