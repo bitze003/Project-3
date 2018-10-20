7c69d85412bd35d352baab9ea48484ac424ad976
wood0761 <wood0761@gmail.com>
2018-10-20 13:00:25 -0500

    mongo db in config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    db: 'mongodb://username:password@url:port/db',
+    //'mongodb://username:password@url:port/db'
+    db: 'mongodb://<dbuser>:<dbpassword>@ds237563.mlab.com:37563/heroku_wmzkpgjk',
     db_dev: 'mongodb://localhost:27017/login',
   };
   
